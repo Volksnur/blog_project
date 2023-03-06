286149d653e98db371f852e2ae947e21c1d29212
Tyler <volksnur@gmail.com>
2023-03-06 15:49:55 -0500

    initial commit of Blog Proje ct

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//let newMail = document.getElementById("contact");
+//mail = document.getElementsByName("submit");
+mail.href = "thmaskell@oakland.edu";
+mail.click();
+function submitEmail() {
+  mail.href = "thmaskell@oakland.edu";
+  mail.submitEmail();
+}
